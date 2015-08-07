7e91f77914af45bf448ecf9c926419d9611b3e75
Cody Bouscaren <codybousc@gmail.com>
2015-08-07 12:08:22 -0700

    Bonfires 7 & 8

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -2,7 +2,7 @@
 
 
 
-//Find the Longest Word in a String
+// Bonfire #6 Find the Longest Word in a String
 
 function findLongestWord(str) {
   strArray = str.split(" ");
@@ -18,7 +18,27 @@ function findLongestWord(str) {
   }
 
     var largest = Math.max.apply(Math, trackLengthArr);
-    console.log(largest);
+    return largest;
 }
 
 findLongestWord('namedhenry The quick brown fox namedhenry jumped over the lazy dog ');
+
+
+// Bonfire #7 Upcase first letter of each word in a String
+
+function titleCase(str) {
+
+  var firstLetter = "";
+  var words = str.toLowerCase().split(" ");
+
+  for (n = 0; n < words.length; n++)
+    {
+      firstLetter = words[n].charAt(0);
+      words[n] = words[n].replace(firstLetter, firstLetter.toUpperCase());
+    }
+  return words.join(" ");
+}
+titleCase("I'm a little tea pot");
+
+
+//Bonfire #8 Return Largest Numbers in Arrays
