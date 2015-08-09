8e2fcd979a2dc4883bd9b624422459308321a60b
Cody Bouscaren <codybousc@gmail.com>
2015-08-09 10:14:15 -0700

    bonfire #8

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -41,4 +41,20 @@ function titleCase(str) {
 titleCase("I'm a little tea pot");
 
 
-//Bonfire #8 Return Largest Numbers in Arrays
+//Bonfire #8 Return Largest Numbers in Nested Array
+
+randomArr = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
+
+function largestOfFour(arr) {
+newArr = [];
+for(i = 0; i <= 3; i++) {
+    sortedArr = arr[i].sort(function(a,b){
+        return a - b;
+    });
+    newArr.push(arr[i].pop());
+
+}
+  return newArr;
+}
+
+console.log(largestOfFour(randomArr));
