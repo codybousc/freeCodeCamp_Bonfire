d713761f60d86662d3a51578b19892a72da4b2cd
Cody Bouscaren <codybousc@gmail.com>
2015-10-13 17:46:38 -0400

    title case

--- freeCodeCamp_deux.js
+++ freeCodeCamp_deux.js
@@ -18,7 +18,7 @@ function palindrome(str) {
   str = str.replace(/\s/g, '').toLowerCase();
   str = str.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     if (finalString == str) {
-        return true; 
+        return true;
     }
     else {
         return false
@@ -26,3 +26,42 @@ function palindrome(str) {
 }
 
 console.log(palindrome("A man, a plan, a canal. Panama"));
+
+#3//Come Back to
+
+#5
+function findLongestWord(str) {
+   str = str.split(" ");
+   var i = 0;
+   var j = 1;
+   longestWordArr = str[i];
+     for(i; i < str.length; i++) {
+         firstWord = str[i];
+        console.log("firstWord = " + firstWord);
+        for (j; j < str.length; j++) {
+            secondWord = str[j];
+
+		}//inner closing loop
+        console.log("secondWord = " + secondWord);
+    }//outer closing bracket
+            console.log("LONGEST WORD = " + longestWordArr);
+
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
+#6
+function titleCase(str) {
+
+  var firstLetter = "";
+  var words = str.toLowerCase().split(" ");
+  console.log(words);
+
+  for (n = 0; n < words.length; n++)
+    {
+      firstLetter = words[n].charAt(0);
+      words[n] = words[n].replace(firstLetter, firstLetter.toUpperCase());
+    }
+  return words.join(" ");
+}
+console.log(titleCase("I'm a little tea pot"));
