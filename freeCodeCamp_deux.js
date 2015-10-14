2aad7a023b9f1d03bc5774bd6f5630aec228f5e6
Cody Bouscaren <codybousc@gmail.com>
2015-10-14 10:20:21 -0400

    Confirm the Ending

--- freeCodeCamp_deux.js
+++ freeCodeCamp_deux.js
@@ -78,3 +78,19 @@ function largestOfFour(arr) {
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+#8
+function end(str, target) {
+  var targetLength = target.length
+  var subString = str.substr(- targetLength);
+    if (subString == target) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(end("He has to give me a new name", "name"));
+
+#9
