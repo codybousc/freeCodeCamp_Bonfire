67ff2c6f8e4e9f4aeb5911005861c13cc4c31035
Cody Bouscaren <codybousc@gmail.com>
2015-10-19 11:03:55 -0400

    Functioning random quote generator. Could restyle though

--- quoteGenerator/js/main.js
+++ quoteGenerator/js/main.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-  sampler();
+populateRandomQuotes();
 
-var randomQs = ["Sup Larry?! ", "Sup Bob", "How you livin' bro?" ];
+function populateRandomQuotes() {
+  var randomQuotes = [
+    "The aim of life is to live, and to live means to be aware, joyously, drunkenly, serenely, divinely aware.",
+    "I have no money, no resources, no hopes. I am the happiest man alive.",
+    "Chaos is the score upon which reality is written.",
+    "The moment one gives close attention to any thing, even a blade of grass it becomes a mysterious, awesome, indescribably magnificent world in itself",
+    "The only thing we never get enough of is love; and the only thing we never give enough of is love.",
+    "If men cease to believe that they will one day become gods then they will surely become worms.",
+    "Any genuine philosophy leads to action and from action back again to wonder, to the enduring fact of mystery."
+  ];
+  var rand = randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
+  $('.quote').text(rand);
 
-function sampler(array) {
-  var rand = array[Math.floor(Math.random() * array.length)];
-$('quote').text(rand);
+
 }
 
-$("button").on("click", function(){
-  sampler(randomQs);
+
+$("button").click(function() {
+  populateRandomQuotes();
 });
+
+
+
 });
